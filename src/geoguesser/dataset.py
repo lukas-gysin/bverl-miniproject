6dae13229f10bf4b0cbcbf744eb24005597e40a3
Lukas Gysin <lukas.gysin@students.fhnw.ch>
2025-01-02 20:42:49 +0100

    Create Dataset directly from download

--- src/geoguesser/dataset.py
+++ src/geoguesser/dataset.py
@@ -10,8 +10,35 @@ from PIL import Image
 from torch.utils.data import Dataset
 
 class EuroSATDataset(Dataset):
-  @staticmethod
-  def download(download_dir: Path = Path('/workspace/code/data')):
+  def __init__(self, root_dir, transform: Callable | None = None,):
+    self.root_dir = root_dir
+    self.transform = transform
+
+    self.observations = list()
+    self.classes = os.listdir(root_dir)
+    for label in self.classes:
+        class_dir = os.path.join(root_dir, label)
+        for img_name in os.listdir(class_dir):
+            img_path = os.path.join(class_dir, img_name)
+            if img_path.lower().endswith('.jpg'):
+                self.observations.append({"image_path": img_path, "label": label})
+
+  def __len__(self):
+    return len(self.observations)
+
+  def __getitem__(self, idx):
+    image_path = self.observations[idx]["image_path"]
+    image = Image.open(image_path)
+    label = self.observations[idx]["label"]
+    label_num = self.classes.index(label)
+
+    if self.transform:
+      image = self.transform(image)
+
+    return {"image": image, "label": label_num}
+
+  @classmethod
+  def download(cls, download_dir: Path = Path('/workspace/code/data')):
     url = 'https://zenodo.org/records/7711810/files/EuroSAT_RGB.zip?download=1'
     save_path = download_dir / 'rgb.zip'
     extract_path = download_dir / 'rgb/'
@@ -40,32 +67,7 @@ class EuroSATDataset(Dataset):
           zip_ref.extractall(extract_path)
       print(f"File extracted to {extract_path}")
 
-  def __init__(self, root_dir, transform: Callable | None = None,):
-    self.root_dir = root_dir
-    self.transform = transform
-
-    self.observations = list()
-    self.classes = os.listdir(root_dir)
-    for label in self.classes:
-        class_dir = os.path.join(root_dir, label)
-        for img_name in os.listdir(class_dir):
-            img_path = os.path.join(class_dir, img_name)
-            if img_path.lower().endswith('.jpg'):
-                self.observations.append({"image_path": img_path, "label": label})
-
-  def __len__(self):
-    return len(self.observations)
-
-  def __getitem__(self, idx):
-    image_path = self.observations[idx]["image_path"]
-    image = Image.open(image_path)
-    label = self.observations[idx]["label"]
-    label_num = self.classes.index(label)
-
-    if self.transform:
-      image = self.transform(image)
-
-    return {"image": image, "label": label_num}
+    return cls(root_dir=extract_path / 'EuroSAT_RGB/')
 
   @classmethod
   def from_subset(cls, original, indices: list[int], transform: Callable | None = None):
